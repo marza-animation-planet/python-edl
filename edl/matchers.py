cd99310cb249890759e800b6bc9357ca0cf5b7be
AdricEpic <adric.worley@epicgames.com>
2015-12-15 09:32:45 -0500

    Rename Matcher.apply's "stack" variable to "edl". "Stack" did not make it immediately obvious that it was an EDL object.

--- edl/matchers.py
+++ edl/matchers.py
@@ -15,7 +15,7 @@ class Matcher(object):
     def matches(self, line):
         return re.match(self.regex, line)
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         sys.stderr.write("Skipping:" + line)
         return True
 
@@ -26,10 +26,10 @@ class TitleMatcher(Matcher):
     def __init__(self):
         Matcher.__init__(self, 'TITLE: (.+)')
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         m = re.search(self.regex, line)
         try:
-            stack.title = m.group(1).strip()
+            edl.title = m.group(1).strip()
             return True
         except (IndexError, AttributeError):
             return False
@@ -42,16 +42,16 @@ class CommentMatcher(Matcher):
     def __init__(self):
         Matcher.__init__(self, '\*\s*(.+)')
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         #print line
         m = re.search(self.regex, line)
         if m:
             # TODO: Handle comments that are not tied to an event
-            if len(stack) > 0:
-                stack[-1].comments.append("* " + m.group(1))
+            if len(edl) > 0:
+                edl[-1].comments.append("* " + m.group(1))
                 mo = re.search('\*\s+FROM\s+CLIP\s+NAME:\s+(.+)', line)
                 if mo:
-                    stack[-1].clip_name = mo.group(1).strip()
+                    edl[-1].clip_name = mo.group(1).strip()
                 return True
         else:
             return False
@@ -64,7 +64,7 @@ class FallbackMatcher(Matcher):
     def __init__(self):
         Matcher.__init__(self, '/^(\w)(.+)/')
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         return True
 
 
@@ -77,10 +77,10 @@ class NameMatcher(Matcher):
         #       add a test for this
         Matcher.__init__(self, '\*\s*FROM CLIP NAME:(\s+)(.+)')
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         m = re.search(self.regex, line)
-        if m and len(stack) > 0:
-            stack[-1].clip_name = m.group(2).strip()
+        if m and len(edl) > 0:
+            edl[-1].clip_name = m.group(2).strip()
             return True
         else:
             return False
@@ -93,11 +93,11 @@ class SourceMatcher(Matcher):
     def __init__(self):
         Matcher.__init__(self, '\*\s*SOURCE FILE:(\s+)(.+)')
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         m = re.search(self.regex, line)
 
-        if m and len(stack) > 0:
-            stack[-1].source_file = m.group(2).strip()
+        if m and len(edl) > 0:
+            edl[-1].source_file = m.group(2).strip()
             return True
         else:
             return False
@@ -110,10 +110,10 @@ class EffectMatcher(Matcher):
     def __init__(self):
         Matcher.__init__(self, 'EFFECTS NAME IS(\s+)(.+)')
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         m = re.search(self.regex, line)
         if m:
-            stack[-1].transition.effect = m.group(2).strip()
+            edl[-1].transition.effect = m.group(2).strip()
             return True
         else:
             return False
@@ -129,13 +129,13 @@ class TimewarpMatcher(Matcher):
         #self.regexp = 'M2\s+(\S+)\s+(\S+)\s+(\S+)'
         Matcher.__init__(self, self.regexp)
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         m = re.search(self.regexp, line)
         if m:
-            stack[-1].timewarp = \
+            edl[-1].timewarp = \
                 Timewarp(m.group(1), m.group(2), m.group(3), self.fps)
             if float(m.group(2)) < 0:
-                stack[-1].timewarp.reverse = True
+                edl[-1].timewarp.reverse = True
             return True
         else:
             return False
@@ -165,7 +165,7 @@ class EventMatcher(Matcher):
     def stripper(cls, in_string):
         return in_string.strip()
 
-    def apply(self, stack, line):
+    def apply(self, edl, line):
         m = re.search(self.regex, line.strip())
         if m:
             matches = m.groups()
@@ -173,8 +173,8 @@ class EventMatcher(Matcher):
             evt = Event(dict(zip(self._keys, values)))
             t = evt.tr_code
             if t == 'C':
-                if len(stack) > 0:
-                    stack[-1].next_event = evt
+                if len(edl) > 0:
+                    edl[-1].next_event = evt
                 evt.transition = Cut()
             elif t == 'D':
                 evt.transition = Dissolve()
@@ -188,7 +188,7 @@ class EventMatcher(Matcher):
             evt.src_end_tc = timecode.Timecode(self.fps, evt.src_end_tc)
             evt.rec_start_tc = timecode.Timecode(self.fps, evt.rec_start_tc)
             evt.rec_end_tc = timecode.Timecode(self.fps, evt.rec_end_tc)
-            stack.append(evt)
+            edl.append(evt)
             return True
         else:
             return False
